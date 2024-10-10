20454249b38262e5b9efef8acc4187aad333cf49
Francis Rombo <fromb@kali.>
2024-10-10 16:42:40 +0000

    lockboxes

--- 0x01-lockboxes/0-lockboxes.py
+++ 0x01-lockboxes/0-lockboxes.py
@@ -1,9 +1,23 @@
 #!/usr/bin/python3
+"""
+    lockboxes
+"""
+
 
 def canUnlockAll(boxes):
+    """
+    Determines if all boxes can be opened.
+
+    Args:
+    boxes (list of lists): A list where each element
+    is a list of keys contained in that box.
+
+    Returns:
+    bool: True if all boxes can be opened, else False.
+    """
     unlocked = set()
     to_check = [0]
-    
+
     while to_check:
         current_box = to_check.pop()
         if current_box not in unlocked:
@@ -11,5 +25,5 @@ def canUnlockAll(boxes):
             for key in boxes[current_box]:
                 if key not in unlocked:
                     to_check.append(key)
-    
+
     return len(unlocked) == len(boxes)
